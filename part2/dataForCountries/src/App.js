a1abf623c82ad2e72c8c9a672639a4ace28b1291
Angel MC <95378441+AngelMC43@users.noreply.github.com>
2023-04-17 21:03:33 +0200

    2.14 completed

--- part2/dataForCountries/src/App.js
+++ part2/dataForCountries/src/App.js
@@ -1,70 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Filter from "./components/Filter";
-import PersonForm from "./components/PersonsForm";
-import Person from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [newSearch, setNewSearch] = useState("");
-  const [notes, setNotes] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState("");
+  const [single, setSingle] = useState(null);
+
+  const filterCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(search)
+  );
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    axios.get("https://restcountries.com/v2/all").then((response) => {
+      setCountries(response.data);
     });
   }, []);
-  console.log(persons);
 
-  const newNote = { name: newName, number: newNumber, id: persons.length + 1 };
+  const capitalWeather = filterCountries[0]?.capital;
+  const countryWeather = filterCountries[0]?.name;
 
-  const addPerson = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${capitalWeather},${countryWeather}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [search]);
 
-    persons.find((person) => person.name === newName)
-      ? alert(`${newName} already exists`)
-      : alert(
-          `${newName} added to the phonebook`,
-          setPersons(persons.concat(newNote))
-        );
-    setNewName("");
-    setNewNumber("");
-  };
-
-  const handleNoteChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleSearch = (event) => {
-    setNewSearch(event.target.value);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
   };
 
   return (
     <div>
-      <h2>Phonebook</h2>
       <Filter
-        newSearch={newSearch}
-        handleSearch={handleSearch}
-        persons={persons}
+        countries={countries}
+        handleChange={handleChange}
+        search={search}
+        weather={weather}
+        filterCountries={filterCountries}
+        setSingle={setSingle}
+        single={single}
       />
-
-      <h2>Add a new</h2>
-      <PersonForm
-        addPerson={addPerson}
-        newName={newName}
-        handleNoteChange={handleNoteChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
-
-      <h2>Numbers</h2>
-      <Person persons={persons} newSearch={newSearch} />
     </div>
   );
 };
--- part2/dataForCountries/src/components/Filter.js
+++ part2/dataForCountries/src/components/Filter.js
@@ -1,9 +1,92 @@
-export default function Filter({ newSearch, handleSearch }) {
+export default function Filter({
+  search,
+  countries,
+  handleChange,
+  weather,
+  filterCountries,
+  single,
+  setSingle,
+}) {
+  const handleButton = (country) => {
+    setSingle(single === country ? null : country);
+  };
+  console.log(weather);
   return (
     <>
       <div>
-        Filter shown with
-        <input type="search" onChange={handleSearch} value={newSearch} />
+        Find countries
+        <input type="search" onChange={handleChange} value={search} />
+        {filterCountries.length < 11 && filterCountries.length > 1 ? (
+          filterCountries.map((country) => (
+            <li key={country.name}>
+              {country.name}
+              <button onClick={() => handleButton(country.name)}>show</button>
+              {single === country.name && (
+                <>
+                  <div key={country.name}>
+                    <h1>{country.name}</h1>
+                    <p>Capital: {country.capital}</p>
+                    <p>Population: {country.population}</p>
+                    <h2>Languages</h2>
+                    <ul>
+                      {country.languages.map((i) => (
+                        <li key={i.name}>{i.name}</li>
+                      ))}
+                    </ul>
+                    <img src={country.flags?.png} alt="flag" />
+                  </div>
+
+                  <span>
+                    <h2>Weather in {weather.location?.name} </h2>
+                    <p>
+                      Temperature: {weather.current?.temperature}
+                      {weather.request?.unit}
+                    </p>
+                    <img src={weather.current?.weather_icons} alt="icon" />
+                    <p>
+                      Wind: {weather.current?.wind_speed}
+                      direction {weather.current?.wind_dir}
+                    </p>
+                  </span>
+                </>
+              )}
+            </li>
+          ))
+        ) : filterCountries.length === 1 ? (
+          <>
+            <ul>
+              {filterCountries.map((country) => (
+                <div key={country.name}>
+                  <h1>{country.name}</h1>
+                  <p>Capital: {country.capital}</p>
+                  <p>Population: {country.population}</p> <h2>Languages</h2>
+                  <ul>
+                    {filterCountries.map((i) =>
+                      i.languages.map((i) => <li key={i.name}>{i.name}</li>)
+                    )}
+                  </ul>
+                  <img src={country.flags?.png} alt="flag" />
+                </div>
+              ))}
+            </ul>
+            {filterCountries.map((key) => (
+              <span key={key.name}>
+                <h2>Weather in {weather.location?.name} </h2>
+                <p>
+                  Temperature: {weather.current?.temperature}
+                  {weather.request?.unit}
+                </p>
+                <img src={weather.current?.weather_icons} alt="icon" />
+                <p>
+                  Wind: {weather.current?.wind_speed}
+                  direction {weather.current?.wind_dir}
+                </p>
+              </span>
+            ))}
+          </>
+        ) : (
+          <p>Too many matches, specify another filter</p>
+        )}
       </div>
     </>
   );
